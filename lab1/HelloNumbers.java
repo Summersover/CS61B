0058e0dde94a0171af087c6160502c0ba124c8f1
Summersover <wangtuoxin@foxmail.com>
2023-11-18 19:24:38 +0800

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloNumbers {
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.print(x + " ");
+            x = x + 1;
+        }
+    }
 }
